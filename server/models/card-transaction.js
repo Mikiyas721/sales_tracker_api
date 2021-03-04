2b43b3db4bb6c94fef27e180eb50db89e83f14a3
Mikiyas721 <Mikiyas721@gmail.com>
2021-03-04 16:52:31 -0600

    Added two remote methods

--- server/models/card-transaction.js
+++ server/models/card-transaction.js
@@ -1,5 +1,52 @@
 'use strict';
 
-module.exports = function(Cardtransaction) {
+module.exports = function (Cardtransaction) {
 
+  Cardtransaction.addSalesTransaction = async function (card, cash) {
+    const cardResult = await Cardtransaction.create(card);
+    const cashResult = await Cardtransaction.app.models.cashTransaction.create(cash);
+    return {
+      "cardId": cardResult.id,
+      "cashId": cashResult.id,
+      "soldAmount": cardResult.amount,
+      "receivedAmount": cashResult.amount,
+      "salesPersonId": cardResult.salesPersonId,
+      "shopId": cardResult.shopId,
+      "createdAt": cashResult.createdAt,
+      "updatedAt": cashResult.updatedAt
+    }
+  }
+
+  Cardtransaction.remoteMethod('addSalesTransaction', {
+    accepts: [{
+      arg: 'cardTransaction',
+      type: {
+        "amount": "number",
+        "salesPersonId": "string",
+        "shopId": "string",
+        "createdAt": "date",
+        "updatedAt": "date"
+      }
+    }, {
+      arg: 'cashTransaction', type: {
+        "amount": "number",
+        "salesPersonId": "string",
+        "shopId": "string",
+        "createdAt": "date",
+        "updatedAt": "date"
+      }
+    }], returns: {
+      arg: 'sales', type: {
+        "cardId": "string",
+        "cashId": "string",
+        "soldAmount": "number",
+        "receivedAmount": "number",
+        "salesPersonId": "string",
+        "shopId": "string",
+        "createdAt": "date",
+        "updatedAt": "date"
+      }
+    },
+    description: 'Adds Card Transactions and Cash Transactions'
+  });
 };
--- server/models/shop.js
+++ server/models/shop.js
@@ -1,5 +1,41 @@
 'use strict';
 
-module.exports = function(Shop) {
+module.exports = function (Shop) {
+  Shop.addSalespersonShop = async function (shopToSave, salespersonId) {
+    const shopResult = await Shop.findOrCreate({where: {phoneNumber: {eq: `${shopToSave.phoneNumber}`}}}, shopToSave);
+    await Shop.app.models.shopSales.create(
+      {
+        "salesPersonId": salespersonId,
+        "shopId": shopResult.id
+      });
 
+    return shopResult['shop'];
+  }
+  Shop.remoteMethod('addSalespersonShop', {
+    accepts: [
+      {
+        arg: 'shop', type: {
+          "name": "string",
+          "address": "string",
+          "phoneNumber": "string",
+          "balance": "number",
+          "createdAt": "date",
+          "updatedAt": "date"
+        }
+      },
+      {arg: 'salespersonId', type: 'string'}
+    ],
+    returns: {
+      arg: 'shop', type: {
+        "id": "string",
+        "name": "string",
+        "address": "string",
+        "phoneNumber": "string",
+        "balance": "number",
+        "createdAt": "date",
+        "updatedAt": "date"
+      }
+    },
+    description: 'Adds Shop to both shop and shop-sales databases'
+  })
 };
