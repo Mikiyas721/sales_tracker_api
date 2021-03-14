bbd200ace902dd5f1604857be40a330a95002f83
Mikiyas721 <Mikiyas721@gmail.com>
2021-03-14 10:15:00 -0600

    Added Temporary Admin

--- server/boot/seed.js
+++ server/boot/seed.js
@@ -1,3 +1,13 @@
-module.exports = function(server) {
-  // server.models.admin.findOrCreate()
+module.exports = function (server) {
+  server.models.admin.findOrCreate({
+    where: {
+      phoneNumber: {
+        eq: '+251941135730'
+      }
+    }
+  }, {
+    "name": "Mikiyas Tesfaye",
+    "phoneNumber": "+251941135730",
+    "roleId": "string"
+  })
 };
